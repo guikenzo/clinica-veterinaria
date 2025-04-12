d80757acf0d64058895139a95e8ffe5036fca539
Guilherme Sino <guilherme.sino@nubank.com.br>
2025-04-12 14:19:22 -0300

    editing scanner singleton

--- Eng3_Atv01/src/application/ScannerSingleton.java
+++ Eng3_Atv01/src/application/ScannerSingleton.java
@@ -1,5 +1,16 @@
 package application;
 
-public class ScannerSingleton {
+import java.util.Scanner;
 
+public class ScannerSingleton {
+    private static Scanner instance;
+
+    private ScannerSingleton() {}
+
+    public static Scanner getInstance() {
+        if (instance == null) {
+            instance = new Scanner(System.in);
+        }
+        return instance;
+    }
 }
