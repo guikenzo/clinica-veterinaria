40aff9dff0ac59630f85efb9164c1e8b108464da
Guilherme Sino <guilherme.sino@nubank.com.br>
2025-06-28 00:16:48 -0300

    front

---
+++ src/main/java/com/clinica_veterinaria/controller/ViewController.java
@@ -0,0 +1,17 @@
+package com.clinica_veterinaria.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ViewController {
+    @RequestMapping("/home")
+    @ResponseBody
+    public ModelAndView home(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index.html");
+        return  modelAndView;
+    }
+}
